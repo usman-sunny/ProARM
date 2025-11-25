34bd38a6a6c38a3ba3deab883997941cede8314c
Muhammad Usman <pt.musman3@gmail.com>
2025-11-25 15:27:37 +0500

    added search option in select module dropdown

--- core/app/core/src/lib/components/analytics/analytics-report-modal/analytics-report-modal.component.ts
+++ core/app/core/src/lib/components/analytics/analytics-report-modal/analytics-report-modal.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { StupidDataService } from '../../../services/stupid-data/stupid-data.service';
 import { ActivatedRoute } from '@angular/router';
+import { map, take } from 'rxjs/operators'; 
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
     selector: 'scrm-analytics-report-modal',
@@ -12,6 +14,8 @@ export class AnalyticsReportModalComponent implements OnInit {
     @Output() closeModal = new EventEmitter<void>();
 
     modules$ = this.stupidService.nltAllModules$;
+    searchModule: string = '';
+    filteredModules$ = new BehaviorSubject<any[]>([]);
     selectedModuleName: string = '';
     selectedModuleLabel: string = '';
 
@@ -22,8 +26,10 @@ export class AnalyticsReportModalComponent implements OnInit {
 
     ngOnInit(): void {
         this.collectionId = this.route.snapshot.paramMap.get('collectionId');
-        this.stupidService.getNltAllModules().subscribe();
-        
+
+        this.stupidService.getNltAllModules().subscribe(mods => {
+            this.filteredModules$.next(mods);
+        });
     }
 
     createNewReport(): void {
@@ -46,6 +52,19 @@ export class AnalyticsReportModalComponent implements OnInit {
     selectModule(name: string, label: string): void {
         this.selectedModuleName = name;
         this.selectedModuleLabel = label;
+    }
+
+    searchModules(): void {
+        const term = this.searchModule.toLowerCase().trim();
+
+        this.modules$
+            .pipe(take(1))
+            .subscribe(modules => {
+                const filtered = modules.filter(m => 
+                    m.label?.toLowerCase().includes(term)
+                );
+                this.filteredModules$.next(filtered);
+            });
     }
 
 }
